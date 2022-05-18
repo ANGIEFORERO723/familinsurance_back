160f80022f5e406dd08c7e1f1506300c9afaa40e
ANGIEFORERO723 <angieforero723@gmail.com>
2022-05-18 18:40:42 -0500

    se carga la base de datos

--- src/main/java/com/familinsurance/familinsurance/ServiceController.java
+++ src/main/java/com/familinsurance/familinsurance/ServiceController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+
 @CrossOrigin("*")
 @RestController
 public class ServiceController {
