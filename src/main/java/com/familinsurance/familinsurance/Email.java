9c3fefd31bacf9c1642731cd225e70055881ba4d
unknown <ladypaola1202@gmail.com>
2022-06-22 20:22:17 -0500

    Envio email

---
+++ src/main/java/com/familinsurance/familinsurance/Email.java
@@ -0,0 +1,5 @@
+package com.familinsurance.familinsurance;
+
+public class Email {
+
+}
--- src/main/java/com/familinsurance/familinsurance/ServiceController.java
+++ src/main/java/com/familinsurance/familinsurance/ServiceController.java
@@ -1,12 +1,26 @@
 
 package com.familinsurance.familinsurance;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+//import org.apache.logging.log4j.spi.LoggerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
@@ -20,6 +34,11 @@ public class ServiceController {
     @GetMapping("/")
     public @ResponseBody Iterable<Service> getServices() {
         return serviceRepository.findAll();
+    }
+    @GetMapping("/send")
+    public @ResponseBody String send() {
+        sendEmail("jonathanalexandergonzalezleon@gmail.com","Servicios de nuestra empresa","ladypaola1202@gmail.com","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");
+        return "enviado";
     }
 
     @PostMapping(value = "/")
@@ -55,4 +74,48 @@ public class ServiceController {
         responseDto_.message="Datos recibidos correctamente";
         return responseDto_;
     }
+    void sendEmail(
+        String emailFrom,
+        String subject,
+        String toEmail,
+        String sendGridKey
+      ) {
+        Email from = new Email(emailFrom);
+    
+        Email to = new Email(toEmail);
+        Content content = new Content("text/html", getHtmlTemplate());
+        Mail mail = new Mail(from, subject, to, content);
+    
+        SendGrid sg = new SendGrid(sendGridKey);
+        Request request = new Request();
+        try {
+          request.setMethod(Method.POST);
+          request.setEndpoint("mail/send");
+          request.setBody(mail.build());
+          Response response = sg.api(request);
+          System.out.println(response.getStatusCode());
+          System.out.println(response.getBody());
+          System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+          System.out.println(ex.toString());
+        }
+      }
+      
+      String getHtmlTemplate() {
+        String response = "";
+        try {
+          InputStream is = new ClassPathResource("/plantilla.html")
+            .getInputStream();
+          String contents = new String(
+            FileCopyUtils.copyToByteArray(is),
+            StandardCharsets.UTF_8
+          );
+          response = contents;
+        } catch (IOException ex) {
+          Logger
+            .getLogger(ServiceController.class.getName())
+            .log(Level.SEVERE, null, ex);
+        }
+        return response;
+      }
 }
